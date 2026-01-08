bcef83c600fb89ad90431f72e9ac01466b36bac7
BartProjectCode <bisliwa@myges.fr>
2026-01-08 21:40:48 +0100

    Modification mineur de certains paramètre et level design mineur

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform playerTransform;
-    
+    private Vector3 offset = new Vector3(0, 1f, 0);
+
     void Update()
     {
-        transform.position = playerTransform.position;
+        transform.position = playerTransform.position - offset;
     }
 }
